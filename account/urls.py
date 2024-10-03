66e8af3f756f3d3ab447f6e81c0086a98c946378
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-10-03 15:23:13 -0300

    password change

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),    # Para cambiar la contraseña
+    path('password-change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),    # Para mostrar mensaje de éxito de cambio contraseña
+        path('', views.dashboard, name='dashboard'),
 ]
